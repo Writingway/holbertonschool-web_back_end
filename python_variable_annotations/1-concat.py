b38b7a16da737faeb5fe22188c7d74a4d174066c
Writingway <sendmeat4time@gmail.com>
2026-04-08 09:42:57 +0200

    feat: update module docstring for concat function to clarify its purpose

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+This module defines a function `concat` that takes two strings as arguments
+and returns their concatenation.
+"""
+
 
 def concat(str1: str, str2: str) -> str:
     """Return a concatenated string."""
