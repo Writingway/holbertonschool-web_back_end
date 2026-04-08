f699fd0f2217fa9a691b116a3079a71be87f5385
Writingway <sendmeat4time@gmail.com>
2026-04-08 11:01:49 +0200

    fix: remove unnecessary docstring arguments in wait_n function for clarity

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,11 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times with the specified max_delay.
-    Args:
-        n (int): The number of times to spawn wait_random.
-        max_delay (int): The maximum delay in seconds for wait_random.
-    Returns:
-        List[float]: A list of all the delays (float values) in ascending order.
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
