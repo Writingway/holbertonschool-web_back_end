f2aa26494ff4059da2c51d2a728902c65dcbb50e
Writingway <sendmeat4time@gmail.com>
2026-04-28 10:03:26 +0200

    fix: optimize string concatenation in taskNext function

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -9,5 +9,7 @@ export function getLast() {
 
 export function taskNext() {
   let combination = 'But sometimes let';
-  return combination + getLast();
+  combination += getLast();
+
+  return combination;
 }
