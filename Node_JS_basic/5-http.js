1db99195d63db7eafb612a1cf3fb88956a1e247a
Writingway <sendmeat4time@gmail.com>
2026-05-12 13:52:13 +0200

    fix: update response message format for student list endpoint

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,7 +6,7 @@ const app = http.createServer((req, res) => {
     countStudents(process.argv[2])
       .then((result) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(result);
+        res.end('This is the list of our students\n' + result);
       })
       .catch((error) => {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
