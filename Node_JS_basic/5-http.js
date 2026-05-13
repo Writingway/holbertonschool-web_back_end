acf4682dc2dce53c73a5c8afb586e13af26b3fdf
Writingway <sendmeat4time@gmail.com>
2026-05-13 10:24:47 +0200

    refactor: restructure HTTP server response handling and improve student list formatting

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,23 +1,43 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8').then((data) => {
+    const lines = data.split('\n').filter((l) => l.trim() !== '');
+    const students = lines.slice(1); // skip header
+
+    if (students.length === 0) throw new Error('No students found');
+
+    const fields = {};
+    for (const line of students) {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstname);
+    }
+
+    let output = `Number of students: ${students.length}`;
+    for (const [field, names] of Object.entries(fields)) {
+      output += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+    }
+    return output;
+  });
+}
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/students') {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     countStudents(process.argv[2])
       .then((result) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(result);
+        res.end(`This is the list of our students\n${result}`);
       })
-      .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(error.message);
+      .catch((err) => {
+        res.end(`This is the list of our students\n${err.message}`);
       });
-  } else if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
   }
 });
 
 app.listen(1245);
-
 module.exports = app;
